eefd9aa63a3024e5d5a0cce977e544f44619c48d
Hai La <Lahai7744@gmail.com>
2021-11-20 16:53:05 +0700

    first project

---
+++ src/main/java/QLSach/testDocGia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QLSach;
+
+/**
+ *
+ * @author Manh Hai
+ */
+public class testDocGia {
+    public static void main(String[] args) {
+        DocGia dg1 = new DocGia();
+        dg1.input();
+        dg1.output();
+    }
+}
+
